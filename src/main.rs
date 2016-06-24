ebede59c9c53bd1be25f0fe0481dbb33cadb187d
Chris C Cerami <chrisccerami@gmail.com>
2016-06-23 20:29:26 -0400

    Serve json over http

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use std::fs::File;
-use std::io::{BufRead,BufReader,Write};
+use std::io::{BufRead,BufReader};
 use std::collections::HashMap;
 extern crate rustc_serialize;
 use rustc_serialize::json;
+use rustc_serialize::json::EncoderError;
 extern crate regex;
 use regex::Regex;
+extern crate hyper;
+use hyper::server::{Server, Request, Response};
+use hyper::header::AccessControlAllowOrigin;
 
 fn main() {
+    Server::http("0.0.0.0:9876").unwrap().handle(handler).unwrap();
+}
+
+fn handler(_: Request, mut response: Response) {
+    response.headers_mut().set(AccessControlAllowOrigin::Any);
+    response.send(response_json().unwrap().as_bytes()).unwrap();
+}
+
+fn response_json() -> Result<String, EncoderError> {
     let mut counted_files = most_often_changed_files();
     counted_files.truncate(100);
-    let mut json_file = File::create("git-log.json").unwrap();
-    if let Ok(json) = json::encode(&counted_files) {
-        match json_file.write(json.as_bytes()) {
-            Ok(_) => println!("Most commonly changed files written to git-log.json"),
-            Err(_) => println!("Could not write to git-log.json")
-        }
-    }
+    json::encode(&counted_files)
 }
 
 #[derive(RustcEncodable)]
